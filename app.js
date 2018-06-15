5e4f861cf5072dc5428d925f055bf226c6de43ed
Jerome Biggins <jeromeebiggins@gmail.com>
2018-06-15 01:07:05 -0500

    Added files via upload
    Changes to the HTML, CSS and JS files

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
 /*
  * Create a list that holds all of your cards
  */
+const symbols = ["fa fa-diamond", "fa fa-diamond", "fa fa-paper-plane-o", "fa fa-paper-plane-o",
+               "fa fa-anchor", "fa fa-anchor", "fa fa-bolt", "fa fa-bolt", "fa fa-cube", "fa fa-cube",
+               "fa fa-leaf", "fa fa-leaf", "fa fa-bicycle", "fa fa-bicycle", "fa fa-bomb", "fa fa-bomb"];
 
 
+const cardsContainer = document.querySelector(".deck");
+
+// create the cards
+
+for(let i = 0; i < symbols.length; i++) {
+  const card = document.createElement("li");
+  card.classList.add("card");
+  card.innerHTML = `<i class="${symbols[i]}"></i>`;
+  cardsContainer.appendChild(card);
+
+  card.addEventListener("click", function() {
+    console.log(card.innerHTML);
+  });
+}
 /*
  * Display the cards on the page
  *   - shuffle the list of cards using the provided "shuffle" method below
