9d94c5956109ac0a96bd670a0002d98cc5441a0d
Jerome Biggins <jeromeebiggins@gmail.com>
2018-06-18 02:18:53 -0500

    Added files via upload
    Removed the list elements in HTML and created an array of those elements in app.js.

--- app.js
+++ app.js
@@ -1,30 +1,13 @@
 /*
  * Create a list that holds all of your cards
  */
+const cards = ['fa-diamond', 'fa-diamond', 'fa-paper-plane-o', 'fa-paper-plane-o', 'fa-anchor', 'fa-anchor',
+               'fa-bolt', 'fa-bolt', 'fa-cube', 'fa-cube', 'fa-leaf', 'fa-leaf', 'fa-bicycle', 'fa-bicycle',
+               'fa-bomb', 'fa-bomb'];
 
- // Display all the cards
-const cards = document.querySelectorAll('.card');
-const openCards = []; // keeps a count of how many cards are opened
-
-
-// Detect when a card is clicked
-cards.forEach(function(card) {
-  card.addEventListener('click', function(e) {
-    openCards.push(card);
-    card.classList.add('open');
-    card.classList.add('show');
-    console.log('Open Cards: ', openCards.length); // shows how many cards have been opened
-
-    if (openCards.length == 2) {
-      setTimeout(function() {
-        openCards.forEach(function(card) {
-          card.classList.remove('open');
-          card.classList.remove('show');
-        });
-      }, 1000);
-    }
-  });
-});
+function drawCard(card) {
+  return `<li class="card"><i class="fa ${card}"></i></li>`;
+}
 
 /*
  * Display the cards on the page
@@ -59,3 +42,57 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+
+function startGame() {
+  let deck = document.querySelector('.deck');
+  let cardHTML = shuffle(cards).map(function(card) {
+    return drawCard(card);
+  });
+
+  deck.innerHTML = cardHTML.join('');
+}
+
+startGame();
+
+// Display all the cards
+const allCards = document.querySelectorAll('.card');
+const openCards = []; // keeps a count of how many cards are opened
+
+
+// Detect when a card is clicked
+allCards.forEach(function(card) {
+  card.addEventListener('click', function(e) {
+
+    if (!card.classList.contains('open') && !card.classList.contains('show') && !card.classList.contains('match')) {
+      openCards.push(card);
+      card.classList.add('open');
+      card.classList.add('show');
+
+      // Check if 2 cards match
+      if (openCards.length == 2) {
+          if (openCards[0].dataset.card == openCards[1].dataset.card) {
+            openCards[0].classList.add('match');
+            openCards[0].classList.add('open');
+            openCards[0].classList.add('show');
+
+            openCards[1].classList.add('match');
+            openCards[1].classList.add('open');
+            openCards[1].classList.add('show');
+
+            //openCards = [];
+
+          } else {
+            // Hide the cards if there's no match
+            setTimeout(function() {
+              openCards.forEach(function(card) {
+                card.classList.remove('open');
+                card.classList.remove('show');
+                openCards = [];
+              }, 1000);
+
+            });
+          }
+        }
+      }
+    });
+  });
