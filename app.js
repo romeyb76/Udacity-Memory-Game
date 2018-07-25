b20a5a56c3661281d8d546b927c981b3474ba95f
Jerome Biggins <jeromeebiggins@gmail.com>
2018-07-24 23:28:29 -0500

    Add files via upload
    Made some amendments to the Memory game.  I can match the cards now, but need to have the clock stop, modal appear and enable replay.

--- app.js
+++ app.js
@@ -1,13 +1,71 @@
-/*
- * Create a list that holds all of your cards
- */
-const cards = ['fa-diamond', 'fa-diamond', 'fa-paper-plane-o', 'fa-paper-plane-o', 'fa-anchor', 'fa-anchor',
-               'fa-bolt', 'fa-bolt', 'fa-cube', 'fa-cube', 'fa-leaf', 'fa-leaf', 'fa-bicycle', 'fa-bicycle',
-               'fa-bomb', 'fa-bomb'];
+const cards = document.querySelectorAll('.card');
+console.log(cards);
+let toggledCards = [];
+let moves = 0; 
+let clockOff = true;
+let time = 0;
+let clockId; 
+let matched = 0;
+const TOTAL_PAIRS = 8;
 
-function drawCard(card) {
-  return `<li class="card"><i class="fa ${card}"></i></li>`;
+const deck = document.querySelector('.deck');
+deck.addEventListener('click', event => {
+	const clickTarget = event.target;
+	if (clickTarget.classList.contains('card') && toggledCards.length < 2 && !toggledCards.includes(clickTarget)) {
+		toggleCard(clickTarget); 
+		addToggleCard(clickTarget);
+		if (toggledCards.length === 2) {
+			checkForMatch(clickTarget);
+			addMove();
+			checkScore();
+		}
+	} 
+	if(isClickValid(clickTarget)) {
+		if(clockOff) {
+			startClock();
+			clockOff = false;
+		}
+	}
+}); 
+
+if (matched === TOTAL_PAIRS) {
+	gameOver();
 }
+
+function toggleCard(card) {
+	card.classList.toggle('open');
+	card.classList.toggle('show');
+} 
+
+function addToggleCard(clickTarget) {
+	toggledCards.push(clickTarget);
+	console.log(toggledCards);
+} 
+
+function checkForMatch() {
+	if (toggledCards[0].firstElementChild.className ===
+		toggledCards[1].firstElementChild.className) {
+			toggledCards[0].classList.toggle('match');
+			toggledCards[1].classList.toggle('match');
+			toggledCards = [];
+			matched++;
+		}else{
+			setTimeout(() => {
+				toggleCard(toggledCards[0]);
+				toggleCard(toggledCards[1]);
+				toggledCards = [];
+		}, 1000);
+	} 	
+}
+
+function isClickValid(clickTarget) {
+	return (
+		clickTarget.classList.contains('card') &&
+		!clickTarget.classList.contains('match') &&
+		toggledCards.length < 2 &&
+		!toggledCards.includes(clickTarget)
+	);
+} 
 
 /*
  * Display the cards on the page
@@ -32,67 +90,170 @@ function shuffle(array) {
 }
 
 
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
+//Shuffle Cards
+function shuffleDeck() {
+	const cardsToShuffle = Array.from(document.querySelectorAll('.deck li'));
+	const shuffledCards = shuffle(cardsToShuffle);
+	for (card of shuffledCards) {
+		deck.appendChild(card);
+	}
+} 
+shuffleDeck(); 
 
-function startGame() {
-  let deck = document.querySelector('.deck');
-  let cardHTML = shuffle(cards).map(function(card) {
-    return drawCard(card);
-  });
+// Number of moves
+function addMove() {
+	moves++;
+	const movesText = document.querySelector('.moves');
+	movesText.innerHTML = moves;
+} 
 
-  deck.innerHTML = cardHTML.join('');
+
+
+// Stars
+function hideStar() {
+	const starList = document.querySelectorAll('.stars li');
+	for (star of starList) {
+		if (star.style.display !== 'none') {
+			star.style.display = 'none';
+			break;
+		}
+	}
+} 
+
+hideStar();
+hideStar();
+
+function checkScore() {
+	if (moves === 16 || moves === 24) {
+		hideStar();
+	}
+} 
+
+// Clock 
+function startClock() { 
+	    clockId = setInterval(() => {
+		time++;
+		displayTime();
+		console.log(time);
+		
+	}, 1000);
+}
+startClock(); 
+
+function displayTime() {
+	const clock = document.querySelector('.clock');
+	console.log(clock);
+	clock.innerHTML = time;
 }
 
-startGame();
+const minutes = Math.floor(time / 60);
+const seconds = Math.floor(time % 60);
 
-// Display all the cards
-const allCards = document.querySelectorAll('.card');
-const openCards = []; // keeps a count of how many cards are opened
+function stopClock() {
+	clearInterval(clockId);
+} 
 
+/*//Hide Modal
+.hide {
+	display: none;
+} 
 
-// Detect when a card is clicked
-allCards.forEach(function(card) {
-  card.addEventListener('click', function(e) {
+//Toggle modal on and off
+function toggleModal() {
+	const modal = document.querySelector('.modal_background');
+	modal.classList.toggle('hide');
+} 
 
-    if (!card.classList.contains('open') && !card.classList.contains('show') && !card.classList.contains('match')) {
-      openCards.push(card);
-      card.classList.add('open');
-      card.classList.add('show');
+toggleModal(); // Open modal
+toggleModal(); // Close modal 
 
-      // Check if 2 cards match
-      if (openCards.length == 2) {
-          if (openCards[0].dataset.card == openCards[1].dataset.card) {
-            openCards[0].classList.add('match');
-            openCards[0].classList.add('open');
-            openCards[0].classList.add('show');
+// Modal Stats
 
-            openCards[1].classList.add('match');
-            openCards[1].classList.add('open');
-            openCards[1].classList.add('show');
+time = 121;
+displayTime();
+moves = 16;
+checkScore();
 
-            //openCards = [];
+toggleModal();
 
-          } else {
-            // Hide the cards if there's no match
-            setTimeout(function() {
-              openCards.forEach(function(card) {
-                card.classList.remove('open');
-                card.classList.remove('show');
-                openCards = [];
-              }, 1000);
+writeModalStats();
+toggleModal();
 
-            });
-          }
-        }
-      }
-    });
-  });
+function writeModalStats() {
+	const timeStat = document.querySelector('.modal_time');
+	const clockTime = document.querySelector('.clock').innerHTML;
+	const movesStat = document.querySelector('.modal_moves');
+	const starsStat = document.querySelector('.modal_stars');
+	const stars = getStars();
+	
+	timeStat.innerHTML = `Time = ${clockTime}`;
+	movesStat.innerHTML = `Moves = ${moves}`;
+	starsStat.innerHTML = `Stars = ${stars}`;
+} 
+
+function getStars() {
+	stars = document.querySelectorAll('.stars li');
+	starCount = 0;
+	for (star of stars) {
+		if(star.style.display !== 'none') {
+			starCount++;
+		}
+	} 
+	console.log(starCount);
+	return starCount;
+}
+
+/*document.querySelector('.button_cancel').addEventListener('click', () => {
+	toggleModal();
+});
+
+document.querySelector('.button_replay').addEventListener('click', () => {
+	console.log('replay');
+});
+
+function resetGame() {
+	resetClock();
+	resetMoves();
+	resetStars();
+	shuffleDeck();
+} 
+
+function resetClock {
+	stopClock();
+	clockOff = true;
+	time = 0;
+	displayTime();
+} 
+
+function resetMoves() {
+	moves = 0;
+	document.querySelector('.moves').innerHTML = moves;
+} 
+
+function resetStars() {
+	stars = 0;
+	const starList = document.querySelectorAll('.stars li');
+	for (star of starList) {
+		star.style.display = 'inline';
+	}
+} 
+
+function gameOver() {
+	stopClock();
+	writeModalStats();
+	toggleModal();
+} 
+
+function replayGame() {
+	resetGame();
+	toggleModal();
+}
+
+function resetCards() {
+	const cards = document.querySelectorAll('.deck li');
+	for(let card of cards) {
+		card.className = 'card';
+	}
+}*/
+/*document.querySelector('.restart').addEventListener('click', resetGame);
+document.querySelector('.button_replay').addEventListener('click', replayGame);*/
